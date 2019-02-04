c5bccec7321aefbb0e3cf9923a7af8f237041908
Revant <saintrx@gmail.com>
2019-02-05 01:15:37 +0700

    Configure company

--- lisp/init-company-mode.el
+++ lisp/init-company-mode.el
@@ -2,7 +2,11 @@
 (require-package 'company)
 (require 'company)
 
-(setq company-tooltip-align-annotations t)
+(setq company-tooltip-align-annotations t
+      company-idle-delay 0
+      company-minimum-prefix-length 1
+      company-selection-wrap-around t)
+
 (add-hook 'prog-mode-hook 'company-mode)
 
 (provide 'init-company-mode)
