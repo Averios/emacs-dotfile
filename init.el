e26488c77ccb1a6b2bf10bbbfa628fdac2b4f1a3
I Putu Pradnyana <saintrx@gmail.com>
2019-02-04 18:48:19 +0700

    Add terraform language support

--- init.el
+++ init.el
@@ -12,7 +12,24 @@
 (require 'init-editing)
 (require 'init-navigation)
 
+;; Language support
+(require 'init-terraform)
+
 (provide 'init)
 
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (terraform-mode projectile smex ido-completing-read+ flycheck rainbow-delimiters golden-ratio atom-one-dark-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
